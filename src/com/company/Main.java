c1577ee2e58619156b132aa771ee072fb57e591d
nedzlam <nedzlamemic@icloud.com>
2019-11-30 21:35:19 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1, j = 1; i<11; i++) {
+            while(j<4) {
+                System.out.printf("%7d", i*j);
+                j++;
+        }
+            System.out.println(" ");
+            j=1;
+        } }}
+
+
